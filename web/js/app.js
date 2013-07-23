0dd21e92d49e345bcdcef2247718e87f63cf6260
Juhana Harmanen <harmanen@me.com>
2013-07-23 16:00:30 +0300

    closes #3

--- web/js/app.js
+++ web/js/app.js
@@ -8,7 +8,7 @@
  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-var vertxApp = angular.module('vertxModule', []).factory('EventBus', function () {
+var vertxApp = angular.module('vertxModule', []).factory('EventBus',function () {
         var EventBus = new vertx.EventBus(window.location.protocol + '//' + window.location.hostname + ':' + window.location.port + '/eventbus');
         EventBus.onopen = function () {
         }
@@ -19,4 +19,189 @@ var vertxApp = angular.module('vertxModule', []).factory('EventBus', function ()
         return EventBus
 
     }
+).factory('WebUtils',function () {
+        var root = {}
+
+        root.clearAlert = function (scope) {
+            scope.alert = {
+                error: '',
+                success: '',
+                info: '',
+                alert: ''
+            }
+        }
+
+        root.getNextDepartmentId = function (departments) {
+            return departments[departments.length - 1] ? departments[departments.length - 1].id + 1 : 1
+        }
+
+        root.getNextEmployeeId = function (departments) {
+            var nextId = 0
+            departments.forEach(function (department) {
+                    nextId = department.employees[department.employees.length - 1] ? department.employees[department.employees.length - 1].id + 1 : 1
+                }
+            )
+            return nextId
+        }
+
+        return root
+    }
+).factory('DepartmentService',function (EventBus) {
+        var root = {}
+
+        root.listDepartments = function () {
+            var departments = []
+            EventBus.onopen = function () {
+                EventBus.send('vertx.mongopersistor', {action: 'find', collection: 'departments', matcher: {}},
+                    function (reply) {
+                        if (reply.status === 'ok') {
+                            reply.results.forEach(function (result) {
+                                    departments.push(new Department(result))
+                                }
+                            )
+                        } else {
+                            console.error('Failed to retrieve departments: ' + reply.message)
+                        }
+                    }
+                )
+            }
+            return departments
+        }
+
+        root.addDepartment = function (departmentJson, callback) {
+            var result = {}
+            result.department = new Department(departmentJson)
+            EventBus.send('vertx.mongopersistor', {action: 'save', collection: 'departments', document: result.department},
+                function (reply) {
+                    if (reply.status === 'ok') {
+                        result.department._id = reply._id
+                        result.success = true
+                    } else {
+                        result.success = false
+                    }
+                    callback(result)
+                }
+            )
+        }
+
+        root.deleteDepartment = function (department, callback) {
+            var result = {}
+            EventBus.send('vertx.mongopersistor', {action: 'delete', collection: 'departments', document: department, matcher: {_id: department._id}},
+                function (reply) {
+                    console.log(reply)
+                    if (reply.status === 'ok') {
+                        result.department = department
+                        result.success = true
+                    } else {
+                        result.success = false
+                    }
+                    callback(result)
+                }
+            )
+        }
+
+        return root
+    }
+).factory('EmployeeService',function (EventBus) {
+        var root = {}
+
+        root.addEmployee = function (employeeJson, callback) {
+            var result = {}
+            result.employee = new Employee(employeeJson)
+            EventBus.send('vertx.mongopersistor', {action: 'update', collection: 'departments', criteria: {_id: employeeJson.department._id}, objNew: {$push: {employees: result.employee}}},
+                function (reply) {
+                    if (reply.status === 'ok') {
+                        result.success = true
+                    } else {
+                        result.success = false
+                    }
+                    callback(result)
+                }
+            )
+        }
+
+        root.deleteEmployee = function (department, employee, callback) {
+            var result = {}
+            result.employee = employee
+            EventBus.send('vertx.mongopersistor', {action: 'update', collection: 'departments', criteria: {_id: department._id}, objNew: {$pull: {employees: {id: employee.id}}}},
+                function (reply) {
+                    console.log(reply)
+                    if (reply.status === 'ok') {
+                        result.success = true
+                    } else {
+                        result.success = false
+                    }
+                    callback(result)
+                }
+            )
+        }
+
+
+        root.changeDepartment = function (oldDepartment, employee, newDepartment, callback) {
+            var result = {}
+            result.employee = employee
+            result.department = newDepartment
+            EventBus.send('vertx.mongopersistor', {action: 'update', collection: 'departments', criteria: {_id: oldDepartment._id}, objNew: {$pull: {employees: {id: employee.id}}}},
+                function (reply) {
+                    if (reply.status === 'ok') {
+                        oldDepartment.employees.splice(oldDepartment.employees.indexOf(employee), 1)
+                        EventBus.send('vertx.mongopersistor', {action: 'update', collection: 'departments', criteria: {_id: newDepartment._id}, objNew: {$push: {employees: employee}}},
+                            function (reply) {
+                                if (reply.status === 'ok') {
+                                    newDepartment.employees.push(employee)
+                                    result.success = true
+                                } else {
+                                    result.success = false
+                                }
+                                callback(result)
+                            }
+                        )
+                    }
+                }
+            )
+        }
+
+        return root
+    }
+).factory('MunicipalityService', function (EventBus) {
+        var root = {}
+
+        root.checkMunicipalities = function (searchObject, response) {
+            EventBus.send('vertx.mongopersistor', {action: 'find', collection: 'municipalities', matcher: {name: {$regex: searchObject.term.concat('.*')}}},
+                function (reply) {
+                    response($.map(reply.results, function (item) {
+                        return {
+                            label: item.name,
+                            value: item.name
+                        }
+                    }))
+                    return response
+                }
+            )
+        }
+
+        return root
+    }
 )
+
+
+function Department(json) {
+    this._id = json._id
+    this.id = json.id
+    this.name = json.name
+    if (json.employees) {
+        this.employees = json.employees
+    } else {
+        this.employees = []
+    }
+}
+
+
+function Employee(json) {
+    this.id = json.id
+    this.firstName = json.firstName
+    this.lastName = json.lastName
+    this.email = json.email
+    this.contractBeginDate = json.contractBeginDate
+    this.municipalityName = json.municipalityName
+}
--- web/js/controllers.js
+++ web/js/controllers.js
@@ -8,36 +8,20 @@
  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-vertxApp.controller('MainCtrl', function ($scope, $filter, EventBus) {
-        $scope.clearAlert = function () {
-            $scope.alert = {
-                error: '',
-                success: '',
-                info: '',
-                alert: ''
-            }
-        }
-
-        EventBus.onopen = function () {
-            $scope.listDepartments()
-        }
+vertxApp.controller('DepartmentCtrl', function ($scope, $filter, WebUtils, DepartmentService) {
 
         $scope.addDepartment = function (departmentJson) {
             if (departmentJson) {
-                departmentJson.id = calculateDepartmentId($scope.departments)
-                var department = new Department(departmentJson)
-                EventBus.send('vertx.mongopersistor', {action: 'save', collection: 'departments', document: department},
-                    function (reply) {
-                        if (reply.status === 'ok') {
-                            $scope.alert.success = $filter('i18n')('departments.add.success', [department.name])
-                            $scope.departments.push(department)
-                            $scope.department = null
-                        } else {
-                            $scope.alert.error = $filter('i18n')('departments.add.error', [department.name])
-                        }
-                        $scope.$apply()
+                departmentJson.id = WebUtils.getNextDepartmentId($scope.departments)
+                DepartmentService.addDepartment(departmentJson, function (result) {
+                    if (result.success) {
+                        $scope.departments.push(result.department)
+                        $scope.alert.success = $filter('i18n')('departments.add.success', [result.department.name])
+                    } else {
+                        $scope.alert.error = $filter('i18n')('departments.add.error', [result.department.name])
                     }
-                )
+                    $scope.$apply()
+                })
             }
         }
 
@@ -46,107 +30,65 @@ vertxApp.controller('MainCtrl', function ($scope, $filter, EventBus) {
                 if (department.employees.length > 0) {
                     $scope.alert.error = $filter('i18n')('departments.delete.error', [department.name])
                     $scope.alert.info = $filter('i18n')('departments.delete.info')
-                    $scope.$apply()
                 } else {
-                    EventBus.send('vertx.mongopersistor', {action: 'delete', collection: 'departments', document: department, matcher: {_id: department._id}},
-                        function (reply) {
-                            if (reply.status === 'ok') {
-                                $scope.alert.success = $filter('i18n')('departments.delete.success', [department.name])
-                                var index = $scope.departments.indexOf(department)
-                                $scope.departments.splice(index, 1)
-                            } else {
-                                $scope.alert.error = $filter('i18n')('departments.delete.error', [department.name])
-                            }
-                            $scope.$apply()
+                    DepartmentService.deleteDepartment(department, function (result) {
+                        if (result.success) {
+                            var index = $scope.departments.indexOf(result.department)
+                            $scope.departments.splice(index, 1)
+                            $scope.alert.success = $filter('i18n')('departments.delete.success', [result.department.name])
+                        } else {
+                            $scope.alert.error = $filter('i18n')('departments.delete.error', [result.department.name])
                         }
-                    )
+                        $scope.$apply()
+                    })
                 }
             }
         }
+    }
+)
+
+vertxApp.controller('EmployeeCtrl', function ($scope, $filter, WebUtils, EmployeeService, MunicipalityService) {
 
         $scope.addEmployee = function (employeeJson) {
             if (employeeJson) {
-                employeeJson.id = calculateEmployeeId($scope.countEmployees())
-                var index = $scope.departments.indexOf(employeeJson.department)
-                var department = $scope.departments[index]
-                var employee = new Employee(employeeJson)
-
-                EventBus.send('vertx.mongopersistor', {action: 'update', collection: 'departments', criteria: {_id: department._id}, objNew: {$push: {employees: employee}}},
-                    function (reply) {
-                        if (reply.status === 'ok') {
-                            $scope.alert.success = $filter('i18n')('employees.add.success', [employee.lastName, employee.firstName])
-                            $scope.departments[index].employees.push(new Employee(employee))
-                            $scope.employee = null
-                        } else {
-                            $scope.alert.error = $filter('i18n')('employees.add.error', [employee.lastName, employee.firstName])
-                        }
-                        $scope.$apply()
+                employeeJson.id = WebUtils.getNextEmployeeId($scope.departments)
+                EmployeeService.addEmployee(employeeJson, function (result) {
+                    if (result.success) {
+                        employeeJson.department.employees.push(result.employee)
+                        $scope.alert.success = $filter('i18n')('employees.add.success', [result.employee.lastName, result.employee.firstName])
+                    } else {
+                        $scope.alert.error = $filter('i18n')('employees.add.error', [result.employee.lastName, result.employee.firstName])
                     }
-                )
+                    $scope.$apply()
+                })
             }
         }
 
         $scope.deleteEmployee = function (department, employee) {
             if (employee) {
-                EventBus.send('vertx.mongopersistor', {action: 'update', collection: 'departments', criteria: {_id: department._id}, objNew: {$pull: {employees: {id: employee.id}}}},
-                    function (reply) {
-                        if (reply.status === 'ok') {
-                            $scope.alert.success = $filter('i18n')('employees.delete.success', [employee.lastName, employee.firstName])
-                            var employeeIndex = department.employees.indexOf(employee)
-                            department.employees.splice(employeeIndex, 1)
-                            $scope.employee = null
-                        } else {
-                            $scope.alert.error = $filter('i18n')('employees.delete.error', [employee.lastName, employee.firstName])
-                        }
-                        $scope.$apply()
-                    }
-                )
-            }
-        }
-
-        $scope.changeEmployeeDepartment = function (oldDepartment, employee, newDepartment) {
-            if (oldDepartment && employee && newDepartment && oldDepartment._id != newDepartment._id) {
-                EventBus.send('vertx.mongopersistor', {action: 'update', collection: 'departments', criteria: {_id: oldDepartment._id}, objNew: {$pull: {employees: {id: employee.id}}}},
-                    function (reply) {
-                        if (reply.status === 'ok') {
-                            var employeeIndex = oldDepartment.employees.indexOf(employee)
-                            oldDepartment.employees.splice(employeeIndex, 1)
-                            EventBus.send('vertx.mongopersistor', {action: 'update', collection: 'departments', criteria: {_id: newDepartment._id}, objNew: {$push: {employees: employee}}},
-                                function (reply) {
-                                    if (reply.status === 'ok') {
-                                        $scope.alert.success = $filter('i18n')('employees.change.success', [employee.lastName, newDepartment.name])
-                                        var index = $scope.departments.indexOf(newDepartment)
-                                        $scope.departments[index].employees.push(new Employee(employee))
-                                        $scope.employee = null
-                                    } else {
-                                        $scope.alert.error = $filter('i18n')('employees.change.error', [employee.lastName, newDepartment.name])
-                                    }
-                                    $scope.$apply()
-                                }
-                            )
-                        } else {
-                            $scope.alert.error = $filter('i18n')('employees.change.error', [employee.lastName, newDepartment.name])
-                        }
-                        $scope.$apply()
-                    }
-                )
-            }
-        }
-
-        $scope.listDepartments = function () {
-            $scope.departments = []
-            EventBus.send('vertx.mongopersistor', {action: 'find', collection: 'departments', matcher: {}},
-                function (reply) {
-                    if (reply.status === 'ok') {
-                        reply.results.forEach(function (result) {
-                            $scope.departments.push(new Department(result))
-                        })
+                EmployeeService.deleteEmployee(department, employee, function (result) {
+                    if (result.success) {
+                        department.employees.splice(department.employees.indexOf(employee), 1)
+                        $scope.alert.success = $filter('i18n')('employees.delete.success', [result.employee.lastName, result.employee.firstName])
                     } else {
-                        console.error('Failed to retrieve departments: ' + reply.message)
+                        $scope.alert.error = $filter('i18n')('employees.delete.error', [result.employee.lastName, result.employee.firstName])
                     }
                     $scope.$apply()
-                }
-            )
+                })
+            }
+        }
+
+        $scope.changeDepartment = function (oldDepartment, employee, newDepartment) {
+            if (oldDepartment && employee && newDepartment && oldDepartment._id != newDepartment._id) {
+                EmployeeService.changeDepartment(oldDepartment, employee, newDepartment, function (result) {
+                    if (result.success) {
+                        $scope.alert.success = $filter('i18n')('employees.change.success', [result.employee.lastName, result.department.name])
+                    } else {
+                        $scope.alert.error = $filter('i18n')('employees.change.error', [result.employee.lastName, result.department.name])
+                    }
+                    $scope.$apply()
+                })
+            }
         }
 
         $scope.checkFirstNames = function (searchObject, response) {
@@ -164,18 +106,16 @@ vertxApp.controller('MainCtrl', function ($scope, $filter, EventBus) {
         }
 
         $scope.checkMunicipalities = function (searchObject, response) {
-            EventBus.send('vertx.mongopersistor', {action: 'find', collection: 'municipalities', matcher: {name: {$regex: searchObject.term.concat('.*')}}},
-                function (reply) {
-                    response($.map(reply.results, function (item) {
-                        return {
-                            label: item.name,
-                            value: item.name
-                        }
-                    }))
-                    return response
-                }
-            )
+            return MunicipalityService.checkMunicipalities(searchObject, response)
         }
+    }
+)
+
+
+vertxApp.controller('MainCtrl', function ($scope, $filter, WebUtils, DepartmentService) {
+        WebUtils.clearAlert($scope)
+
+        $scope.departments = DepartmentService.listDepartments()
 
         $scope.countEmployees = function () {
             var count = 0
@@ -188,38 +128,3 @@ vertxApp.controller('MainCtrl', function ($scope, $filter, EventBus) {
         }
     }
 )
-
-function calculateDepartmentId(departments) {
-    var id = departments.length + 1
-    departments.forEach(function (department) {
-        if (department.id >= id) {
-            id = department.id + 1
-        }
-    })
-    return id
-}
-
-function calculateEmployeeId(amountOfEmployees) {
-    return amountOfEmployees + 1
-}
-
-function Department(json) {
-    this._id = json._id
-    this.id = json.id
-    this.name = json.name
-    if (json.employees) {
-        this.employees = json.employees
-    } else {
-        this.employees = []
-    }
-}
-
-
-function Employee(json) {
-    this.id = json.id
-    this.firstName = json.firstName
-    this.lastName = json.lastName
-    this.email = json.email
-    this.contractBeginDate = json.contractBeginDate
-    this.municipalityName = json.municipalityName
-}
